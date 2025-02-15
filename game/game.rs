c21c13ebba3f4778d83491259dcef47bc154736b
Marcin <marcin.konow@lczyk.xyz>
2025-02-15 16:09:16 +0000

    walking texture (no anim)

--- game/game.rs
+++ game/game.rs
@@ -18,7 +18,8 @@ pub struct State {
     mouse_pos: Vector2,
     mouse_btn: bool,
     music: Option<Music>,
-    font: Option<Font>
+    font: Option<Font>,
+    texture: Option<Texture>,
 }
 
 #[cfg(feature = "web")]
@@ -28,9 +29,9 @@ pub struct State {
     mouse_pos: Vector2,
     mouse_btn: bool,
     music: Option<u32>,
-    font: Option<u32>
+    font: Option<u32>,
+    texture: Option<Texture>,
 }
-
 
 #[cfg(feature = "web")]
 unsafe extern "C" {
@@ -49,7 +50,7 @@ unsafe extern "C" {
         positionY: i32,
         fontSize: i32,
         spacing: f32,
-        tint:*const Color,
+        tint: *const Color,
     );
 }
 
@@ -63,13 +64,25 @@ pub fn DrawTextEx(
     spacing: f32,
     tint: Color,
 ) {
-    unsafe { DrawTextEx_(font, text, position.x as i32, position.y as i32, fontSize as i32, spacing, addr_of!(tint)) }
+    unsafe {
+        DrawTextEx_(
+            font,
+            text,
+            position.x as i32,
+            position.y as i32,
+            fontSize as i32,
+            spacing,
+            addr_of!(tint),
+        )
+    }
 }
 
 #[allow(non_snake_case)]
 pub fn ConsoleLog(msg: String) {
     #[cfg(feature = "web")]
-    unsafe { ConsoleLog_(cstr!(msg)) };
+    unsafe {
+        ConsoleLog_(cstr!(msg))
+    };
     #[cfg(feature = "native")]
     println!("{}", msg);
 }
@@ -77,16 +90,21 @@ pub fn ConsoleLog(msg: String) {
 #[cfg(feature = "native")]
 fn draw_text(font: Option<Font>, text: &str, x: i32, y: i32, size: i32, color: Color) {
     if font.is_none() {
-        unsafe { DrawText(cstr!(text), x, y, size, color); }
+        unsafe {
+            DrawText(cstr!(text), x, y, size, color);
+        }
     } else {
         unsafe {
             DrawTextEx(
                 font.unwrap(),
                 cstr!(text),
-                Vector2 { x: x as f32, y: y as f32 },
+                Vector2 {
+                    x: x as f32,
+                    y: y as f32,
+                },
                 size as f32,
                 2.0,
-                color
+                color,
             );
         }
     }
@@ -95,19 +113,24 @@ fn draw_text(font: Option<Font>, text: &str, x: i32, y: i32, size: i32, color: C
 #[cfg(feature = "web")]
 fn draw_text(font: Option<u32>, text: &str, x: i32, y: i32, size: i32, color: Color) {
     if font.is_none() {
-        unsafe { DrawText(cstr!(text), x, y, size, color); }
+        unsafe {
+            DrawText(cstr!(text), x, y, size, color);
+        }
     } else {
         DrawTextEx(
             font.unwrap(),
             cstr!(text),
-            Vector2 { x: x as f32, y: y as f32 },
+            Vector2 {
+                x: x as f32,
+                y: y as f32,
+            },
             size as f32,
             2.0,
-            color
+            color,
         );
     }
 }
- 
+
 #[no_mangle]
 pub unsafe fn game_init() -> State {
     // SetTargetFPS(300);
@@ -118,37 +141,53 @@ pub unsafe fn game_init() -> State {
     let filename = c"assets/hello_03.wav";
     let music = LoadMusicStream(filename.as_ptr());
 
+    // SetMusicVolume(music, 1.0);
+
     PlayMusicStream(music);
 
-    // let font = LoadFont(cstr!("assets/romulus.png"));
-    // NotoSansJP-Bold.ttf
-    // let font = LoadFont(cstr!("assets/NotoSansJP-Bold.ttf"));
     let font = LoadFont(cstr!("assets/Kavoon-Regular.ttf"));
+
+    // Load slime texture
+    // Blue_Slime-Idle-mag.png
+    let texture = LoadTexture(cstr!("assets/Blue_Slime-Idle-mag.png"));
 
     State {
         rect: Rectangle {
-            x: (WINDOW_WIDTH as f32 - 100.0)/2.0,
-            y: (WINDOW_HEIGHT as f32 - 100.0)/2.0,
+            x: (WINDOW_WIDTH as f32 - 100.0) / 2.0,
+            y: (WINDOW_HEIGHT as f32 - 100.0) / 2.0,
             width: 100.0,
-            height: 100.0
+            height: 100.0,
         },
         speed: 850.0,
         mouse_pos: Vector2 { x: 0.0, y: 0.0 },
         mouse_btn: false,
         music: Some(music),
-        font: Some(font)
+        font: Some(font),
+        texture: Some(texture),
     }
 }
 
 unsafe fn handle_keys(state: &mut State) {
-    if IsKeyDown(KEY::Space)  { state.speed = SPEED_BOOSTED }
-    if !IsKeyDown(KEY::Space) { state.speed = SPEED_DEFAULT }
+    if IsKeyDown(KEY::Space) {
+        state.speed = SPEED_BOOSTED
+    }
+    if !IsKeyDown(KEY::Space) {
+        state.speed = SPEED_DEFAULT
+    }
 
     let dt = GetFrameTime();
-    if IsKeyDown(KEY::W)      { state.rect.y -= dt*state.speed }
-    if IsKeyDown(KEY::A)      { state.rect.x -= dt*state.speed }
-    if IsKeyDown(KEY::S)      { state.rect.y += dt*state.speed }
-    if IsKeyDown(KEY::D)      { state.rect.x += dt*state.speed }
+    if IsKeyDown(KEY::W) {
+        state.rect.y -= dt * state.speed
+    }
+    if IsKeyDown(KEY::A) {
+        state.rect.x -= dt * state.speed
+    }
+    if IsKeyDown(KEY::S) {
+        state.rect.y += dt * state.speed
+    }
+    if IsKeyDown(KEY::D) {
+        state.rect.x += dt * state.speed
+    }
 
     // prevent the rect from wandering off the screen too far
     if state.rect.x < -state.rect.width {
@@ -176,39 +215,65 @@ pub unsafe fn game_frame(state: &mut State) {
     handle_keys(state);
     handle_mouse(state);
 
-    BeginDrawing(); {
+    BeginDrawing();
+    {
         ClearBackground(DARKGREEN);
         draw_text(state.font, "hello world", 250, 500, 50, RAYWHITE);
 
-        DrawRectangleRec(state.rect, RAYWHITE);
+        if state.texture.is_some() {
+            let texture = state.texture.unwrap();
+            let mut position = Vector2 {
+                x: state.rect.x,
+                y: state.rect.y,
+            };
+            let rotation = 0.0;
+            // figure out how to scale the texture to the size of the rect
+            let scale = state.rect.width / texture.width as f32;
 
-        let rect_pos = format!{
+            // Move the texture so it's at the bottom of the rect
+            let scaled_height = texture.height as f32 * scale;
+            position.y += state.rect.height - scaled_height;
+
+            let tint = RAYWHITE;
+            DrawTextureEx(texture, position, rotation, scale, tint);
+        } else {
+            DrawRectangle(
+                state.rect.x as i32,
+                state.rect.y as i32,
+                state.rect.width as i32,
+                state.rect.height as i32,
+                RAYWHITE,
+            );
+        }
+
+        let rect_pos = format! {
             "rect: [{x}, {y}]",
             x = state.rect.x.round(),
             y = state.rect.y.round()
         };
         draw_text(state.font, &rect_pos, 10, 10, 20, RAYWHITE);
 
-        let mouse_pos = format!{
+        let mouse_pos = format! {
             "mouse: [{x}, {y}]",
             x = state.mouse_pos.x.round(),
             y = state.mouse_pos.y.round()
         };
         draw_text(state.font, &mouse_pos, 10, 30, 20, RAYWHITE);
 
-        let color = if state.mouse_btn {
-            RED
-        } else {
-            RAYWHITE
-        };
+        let color = if state.mouse_btn { RED } else { RAYWHITE };
 
-        DrawCircle(state.mouse_pos.x as i32, state.mouse_pos.y as i32, 10.0, color);
-    } EndDrawing();
+        DrawCircle(
+            state.mouse_pos.x as i32,
+            state.mouse_pos.y as i32,
+            10.0,
+            color,
+        );
+    }
+    EndDrawing();
 
     if state.music.is_some() {
         UpdateMusicStream(state.music.unwrap());
     }
-
 }
 
 #[no_mangle]
